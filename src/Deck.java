5c5f53ef1e151b48a079ecba0f79e9b5f30bbdac
Owen Senowitz <owen.senowitz@gmail.com>
2022-03-25 03:22:09 -0400

    not done

--- src/Deck.java
+++ src/Deck.java
@@ -1,2 +1,18 @@
+import java.sql.Array;
+import java.util.*;
+
 public class Deck {
+    public Deck()
+    {
+        List<Card> deck = new ArrayList<>();
+        for (int i = 0; i < Card.SuitType.values().length; i++)
+        {
+            for (int j = 0; j < Card.ValueType.values().length; j++)
+            {
+                Card card = new Card(Card.SuitType.valueOf(String.valueOf(i)), Card.ValueType.valueOf(String.valueOf(j)));
+                deck.add(card);
+            }
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Card card = new Card(Card.SuitType.Hearts, Card.ValueType.Ace);
-        System.out.println(card.toString());
+        Deck deck = new Deck();
+        System.out.println(deck);
     }
 }
